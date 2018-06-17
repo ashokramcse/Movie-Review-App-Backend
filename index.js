860202470438a04b3fed7f94ff0c9fcb5716f457
DESKTOP-FOASAOV\muthu <muthukumar@gridsandguides.com>
2018-06-17 14:25:33 +0530

    CORS Added

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express'); //
 const app = express(); //
 require('dotenv').config();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-app.set('port',process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3000);
+
+app.use(bodyParser.json()); //
+app.use(bodyParser.urlencoded({ extended: true })); //
+app.use(cors());
+
 
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
@@ -10,13 +17,13 @@ mongoose.connect(process.env.DB);
 
 const { Movie } = require('./models/movie');
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.json({
-        message : "Success"
+        message: "Success"
     })
-})
+});
 
-app.get('/get-all-movies',(req,res) => {
+app.get('/get-all-movies', (req, res) => {
     Movie.find()
         .then((movies) => {
             res.json(movies);
@@ -25,11 +32,32 @@ app.get('/get-all-movies',(req,res) => {
         });
 });
 
-app.get('/get-details-by-id/:id',(req,res) => {
+app.get('/get-details-by-id/:id', (req, res) => {
     Movie.findById(req.params.id).then((detail) => {
         res.json(detail);
-    },(err) => {
-        console.log(err)
+    }, (err) => {
+        console.log(err);
+    });
+});
+
+app.put('/add-review/:id', (req, res) => {
+
+    var updateQuery = {
+        $push: {
+            "reviews": req.body
+        }
+    }
+
+    var updateOptions = {
+        "safe": true,
+        new: true
+    }
+
+
+    Movie.findByIdAndUpdate(req.params.id, updateQuery, updateOptions).then((updateFilm) => {
+        res.json(updateFilm);
+    }, (err) => {
+        console.log(err);
     });
 });
 
